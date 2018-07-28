0af7268a7f821286c9ffaf3c2555580a141d4a0e
Alex Dunmow <alex.dunmow@blockninja.io>
2018-07-28 12:10:46 +0800

    removed chunkstring

--- main.go
+++ main.go
@@ -86,7 +86,6 @@ func stripNullCharacter(b []byte) string {
 }
 
 func stripCharacters(s string) string {
-
 	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
 	must(err)
 
@@ -122,22 +121,4 @@ func must(err error) {
 	if err != nil {
 		panic(err) // if one thing doesn't work, this whole thing won't work
 	}
-}
-
-func ChunkString(s string, chunkSize int) []string {
-	var chunks []string
-	runes := []rune(s)
-
-	if len(runes) == 0 {
-		return []string{s}
-	}
-
-	for i := 0; i < len(runes); i += chunkSize {
-		nn := i + chunkSize
-		if nn > len(runes) {
-			nn = len(runes)
-		}
-		chunks = append(chunks, string(runes[i:nn]))
-	}
-	return chunks
 }
